39abdd4422e614b83de169a0911e2af2d8d2734d
Anurak Teerarattananukulchai <anurak.teera@gmail.com>
2020-12-16 23:39:39 +0700

    create data model , implement URLSession to get information from Openweathermap API
    next to do : implement delegate method to use data in the project

---
+++ WeatherApp/Controller/WeatherViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by anurak teerarattananukulchai on 6/12/2563 BE.
+//
+
+import UIKit
+import CoreLocation
+
+class WeatherViewController: UIViewController,UITextFieldDelegate, WeatherManagerDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        weatherManager.delegate = self
+        searchTextField.delegate = self
+    }
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Please enter city name"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        
+        searchTextField.text = ""
+    }
+    
+    func didUpdateWeather(weather: WeatherModel){
+        print(weather.temperature)
+    }
+    
+}
+
---
+++ WeatherApp/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  WeatherApp
+//
+//  Created by anurak teerarattananukulchai on 16/12/2563 BE.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main : Main
+    let weather : [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
